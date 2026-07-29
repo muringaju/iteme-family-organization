4c09383c245668da59ce3cbb03a76af1917032d5
muringaju <muringapr@gmail.com>
2026-07-29 18:26:59 +0200

    Fix Vercel React routing

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 const api = axios.create({
   baseURL: API_URL,
@@ -8,10 +9,12 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("iteme_token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
-
-export const ASSET_BASE = API_URL.replace(/\/api$/, "");
 
 export default api;
